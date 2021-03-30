0f37b109d3c1431a54af82fc97f5727ff3181ee4
TexanElite <sidh@suchdev.com>
2021-03-30 10:36:46 -0500

    Initial commit

---
+++ rsa.py
@@ -0,0 +1,59 @@
+import json
+
+from util import *
+
+
+class RSA:
+
+    def __init__(self, n, e, p=None, q=None, totient=None, d=None):
+        self.n = n
+        self.e = e
+        self.p = p
+        self.q = q
+        self.totient = totient
+        self.d = d
+
+    def update(self):
+        n_bits = len(bin(self.n)[2:])
+        if self.p is None or self.q is None:
+            if self.p is None and self.q is None:
+                if n_bits <= 32:
+                    factors = trial_division_factorizer(self.n)
+                    if len(factors) != 2:
+                        print("Error while updating values: n is not semiprime (n has more than 2 factors)")
+                    else:
+                        self.p = factors[0]
+                        self.q = factors[1]
+                else:
+                    pass
+            elif self.p is not None:
+                self.q = self.n // self.p
+            elif self.q is not None:
+                self.p = self.n // self.q
+        if self.p and self.q:
+            self.totient = (self.p - 1) * (self.q - 1)
+            self.d = mod_inverse(self.e, self.totient)
+
+    def encrypt_message(self, pt):
+        if type(pt) is str:
+            pt = pt.encode('utf-8')
+        pt = bytes_to_long(pt)
+        ct = pow(pt, self.e, self.n)
+        print(ct)
+        ct = long_to_bytes(ct)
+        return ct
+
+    def decrypt_message(self, ct):
+        if self.d is None:
+            print("Error while decrypting message: d is not defined")
+            return
+        if type(ct) is str:
+            ct = ct.encode('utf-8')
+        ct = bytes_to_long(ct)
+        pt = pow(ct, self.d, self.n)
+        pt = long_to_bytes(pt)
+        return pt
+
+    def __repr__(self):
+        dict_form = {'n': self.n, 'e': self.e, 'p': self.p, 'q': self.q, 'totient': self.totient, 'd': self.d}
+        return json.dumps(dict_form)
---
+++ util.py
@@ -0,0 +1,50 @@
+import math
+
+
+def trial_division_factorizer(n):
+    factors = []
+
+    while n % 2 == 0:
+        factors.append(2)
+        n /= 2
+
+    cur_divisor = 3
+
+    while cur_divisor ** 2 <= n:
+        while n % cur_divisor == 0:
+            factors.append(cur_divisor)
+            n //= cur_divisor
+        cur_divisor += 2
+
+    if n != 1:
+        factors.append(n)
+
+    return factors
+
+
+def mod_inverse(n, mod):
+    return (extended_gcd(n, mod)[1] + mod) % mod
+
+
+def extended_gcd(a, b):
+    r0, s0, t0 = a, 1, 0
+    r1, s1, t1 = b, 0, 1
+
+    while r1 != 0:
+        q = r0 // r1
+        r2 = r0 % r1
+
+        s2 = s0 - q * s1
+        t2 = t0 - q * t1
+
+        r0, s0, t0 = r1, s1, t1
+        r1, s1, t1 = r2, s2, t2
+    return r0, s0, t0
+
+
+def bytes_to_long(b):
+    return int.from_bytes(b, 'big')
+
+
+def long_to_bytes(n: int):
+    return n.to_bytes((n.bit_length() + 7) // 8, 'big')
